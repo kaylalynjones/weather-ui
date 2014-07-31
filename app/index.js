a0a3405335eabceb379988e89fed9901a8bce23b
Kayla Jones <KaylaLyne424@gmail.com>
2014-07-31 15:14:38 -0500

    initial files

---
+++ app/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//app.get('/', function(req,res){
+  //res.render('______');
+//});
+
+var port = process.env.PORT;
+app.listen(port, function(){
+  console.log('listening on', port);
+});
